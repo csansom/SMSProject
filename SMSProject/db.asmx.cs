70c7232250652efea815b599ef8a389de047a24f
csansom <csansom@mail.uoguelph.ca>
2020-03-09 13:43:18 -0400

    Log Implenented

--- SMSProject/db.asmx.cs
+++ SMSProject/db.asmx.cs
@@ -36,43 +36,48 @@ namespace SMSProject
                                                combined_entry => combined_entry.farm_cows.AspNetUser_ID,
                                                asp_users => asp_users.Id,
                                                (combined_entry, asp_users) => new {
+                                                   userID = asp_users.Id,
                                                    msgID = combined_entry.z_alerts.id,
                                                    msg = combined_entry.z_alerts.message,
                                                    date = combined_entry.z_alerts.date_emailsent,
                                                    phoneNumber = asp_users.PhoneNumber
                                                });
                 int messagesSent = 0;
+                List<Log> logEntries = new List<Log>();
                 foreach (var row in rows)
                 {
                     if (DateTime.Parse(row.date.ToString()).CompareTo(DateTime.Now.AddMinutes(-30)) >= 0)
                     {
                         // Fill in these feilds.
-                        string login = "your sms feedback ID";
-                        string password = "your sms feedback password";
+                        string login = "your SMS Feedback login";
+                        string password = "your SMS Feedback password";
                         string url = "http://api.smsfeedback.ru/messages/v2/send/?login=" + login + "&password=" + password + "&phone=%2B" + row.phoneNumber + "&text=" + row.msg;
 
                         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                         request.Method = "GET";
-                        var response = request.GetResponse();
-                        messagesSent++;
+                        var response = (HttpWebResponse)request.GetResponse();
+                        string note = "\' encountered an error while sending.";
+                        if (response.StatusCode.ToString().Equals("OK"))
+                        {
+                            messagesSent++;
+                            note = "\' has been sent.";
+                        }
+                        logEntries.Add(new Log
+                        {
+                             id = row.msgID,
+                             user_id = row.userID,
+                             page = "/SMSProject/db.asmx/SendAlerts",
+                             function_query = "insert into log(id, user_id, page, function_query, error, note, datestamp) VALUES(@id, @user_id, @page, @function_query, @error, @note, @datestamp)",
+                             error = response.StatusCode.ToString(),
+                             note = "message:\'" + row.msg + note,
+                             datestamp = DateTime.Now
+                        });
                     }
-                        /*    
-                            using (SqlCommand command = new SqlCommand("set IDENTITY_INSERT dbo.Log on; insert into Log(id, user_id, page, function_query, error, note, datestamp) VALUES(@id, @user_id, @page, @function_query, @error, @note, @datestamp); set IDENTITY_INSERT dbo.Log off;", sqlCon))
-                            {
-                                string note = "Hello " + recipient + ".\n" + row["message"];
-
-                                command.Parameters.AddWithValue("@id", row["id"]);
-                                command.Parameters.AddWithValue("@user_id", "SMSService");
-                                command.Parameters.AddWithValue("@page", "/SMSProject/db.asmx/SendAlerts");
-                                command.Parameters.AddWithValue("@function_query", "insert into log(id, user_id, page, function_query, error, note, datestamp) VALUES(@id, @user_id, @page, @function_query, @error, @note, @datestamp)");
-                                command.Parameters.AddWithValue("@error", "No Error");
-                                command.Parameters.AddWithValue("@note", "message:\'" + note + "\' has been sent.");
-                                command.Parameters.AddWithValue("@datestamp", DateTime.Today);
-
-                                int result = command.ExecuteNonQuery();
-
-                                messagesSent++;
-                            }*/
+                }
+                foreach (Log logRow in logEntries)
+                {
+                    db.Logs.Add(logRow);
+                    db.SaveChanges();
                 }
                 Context.Response.Output.WriteLine(messagesSent + " alert(s) were sent at " + DateTime.Now.ToString() + ".");
             }
